dd2fb085ca152c05e8486498eba0fad8d9700e8e
Ghoster524 <60692171+Ghoster524@users.noreply.github.com>
2021-04-01 10:27:19 +0200

    feat(entering-username): Created page for entering the username

---
+++ Hunted Mobile/Hunted Mobile/Hunted Mobile/View/EnterUsername.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Hunted_Mobile.Model;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Hunted_Mobile.View {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EnterUsername : ContentPage {
+        public bool isValid = false;
+        public Game game = null;
+
+        public EnterUsername(Game game) {
+            InitializeComponent();
+            BindingContext = this;
+
+            this.game = game;
+        }       
+
+        private void HandleJoinGame(object sender, EventArgs e) {
+            this.JoinGameButton.IsEnabled = false;
+            this.Validate();
+
+            if(isValid) {
+                // TODO: Navigate to labby
+                Navigation.PushAsync(new Lobby(), true);
+            }
+
+            this.JoinGameButton.IsEnabled = true;
+        }
+
+        // Check if InviteCode is valid
+        public void Validate() {
+            isValid = this.UserNameField.Text.Length >= 4;
+
+            // Display ErrorMessage
+            this.UserNameMessage.Text = isValid ? "" : "Gebruikersnaam is verplicht en moet minimaal 4 karakters bevatten";
+            OnPropertyChanged(nameof(this.UserNameMessage));
+        }
+
+        // Get game based on InviteCode
+        // TODO: Join a Game, maybe create user
+        public void JoinGame() {
+            //return new Game();
+        }
+    }
+}
