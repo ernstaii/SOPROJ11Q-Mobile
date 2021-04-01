f1bc642b7cbc84b2ebf662459c8dcc1deab7bf55
Ghoster524 <60692171+Ghoster524@users.noreply.github.com>
2021-04-01 10:27:48 +0200

    refactor(MainPage): Using game for entering next screen

--- Hunted Mobile/Hunted Mobile/Hunted Mobile/View/MainPage.xaml.cs
+++ Hunted Mobile/Hunted Mobile/Hunted Mobile/View/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Hunted_Mobile.Model;
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -10,26 +12,44 @@ using Xamarin.Forms;
 namespace Hunted_Mobile.View {
     public partial class MainPage : ContentPage {
         public bool isValid = false;
+        public Game game = null;
 
         public MainPage() {
             this.InitializeComponent();
             BindingContext = this;
         }
 
-        private void HandleJoinGame(object sender, EventArgs e) {
+        // If InviteCode is valid, then user will be redirected to screen for entering an username
+        private void SubmitInviteCode(object sender, EventArgs e) {
+            // Disable the button, so user doesn't click the button twice for loading the game
+            this.SubmitInviteCodeButton.IsEnabled = false;
             this.Validate();
 
-            // TODO: If valid, send to different screen
+            if(isValid) {
+                Navigation.PushAsync(new EnterUsername(this.game), true);
+            }
+
+            this.SubmitInviteCodeButton.IsEnabled = true;
         }
 
+        // Check if InviteCode is valid
         public void Validate() {
-            // TODO: Check if valid
-            isValid = this.InviteCode.Text != "";
+            this.game = GetGame(this.InviteCodeField.Text);
 
-            this.ErrorMessage.Text = isValid ? "" : "De opgegeven code is ongeldig";
-            OnPropertyChanged(nameof(ErrorMessage));
+            isValid = this.game != null;
+
+            // Display ErrorMessage
+            this.InviteCodeMessage.Text = isValid ? "" : "De opgegeven code is ongeldig";
+            OnPropertyChanged(nameof(this.InviteCodeMessage));
         }
 
+        // Get game based on InviteCode
+        // TODO: Check if player is able to get the game, so it is able to validate InviteCode
+        public Game GetGame(string code) {
+            return new Game();
+        }
+
+        // Test button for navigation to MapPage
         private void ToMapButtonClicked(object sender, EventArgs e) {
             Navigation.PushAsync(new MapPage(), true);
         }
