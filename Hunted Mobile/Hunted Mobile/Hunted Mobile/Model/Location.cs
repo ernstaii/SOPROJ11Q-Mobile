434f04041ee5796a19589993b324566871c76824
Luke Derkzen <lderkzen@gmail.com>
2021-04-01 17:38:49 +0200

    Implicit conversion operator for MapsUI.Position to Location

--- Hunted Mobile/Hunted Mobile/Hunted Mobile/Model/Location.cs
+++ Hunted Mobile/Hunted Mobile/Hunted Mobile/Model/Location.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Mapsui.UI.Forms;
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Hunted_Mobile.Model {
-    public class Location {
+    public struct Location {
         public double Lattitude { get; set; }
         public double Longitude { get; set; }
 
+        public Location(double _lattitude, double _longitude) {
+            Lattitude = _lattitude;
+            Longitude = _longitude;
+        }
+
+        public static implicit operator Location (Position p) => new Location(p.Latitude, p.Longitude);
     }
 }
