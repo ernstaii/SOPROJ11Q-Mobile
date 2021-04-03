898ca6f08b0ec7c09d41be52b4e971d11efbb6d2
imfi-jz <qasloot@avans.nl>
2021-04-03 15:02:54 +0200

    Geo location and map viewport restricting
    Center button may take a few seconds to sync with player location.

--- Hunted Mobile/Hunted Mobile/Hunted Mobile/AssemblyInfo.cs
+++ Hunted Mobile/Hunted Mobile/Hunted Mobile/AssemblyInfo.cs
@@ -1,3 +1,9 @@
+using Android.App;
+
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: UsesFeature("android.hardware.location", Required = false)]
+[assembly: UsesFeature("android.hardware.location.gps", Required = false)]
+[assembly: UsesFeature("android.hardware.location.network", Required = false)]
--- Hunted Mobile/Hunted Mobile/Hunted Mobile/ViewModel/MapViewModel.cs
+++ Hunted Mobile/Hunted Mobile/Hunted Mobile/ViewModel/MapViewModel.cs
@@ -2,10 +2,15 @@
 using Hunted_Mobile.Model.Game;
 
 using Mapsui;
+using Mapsui.Geometries;
 using Mapsui.Projection;
+using Mapsui.UI;
 using Mapsui.UI.Forms;
 using Mapsui.Utilities;
 using Mapsui.Widgets;
+
+using Plugin.Geolocator;
+using Plugin.Geolocator.Abstractions;
 
 using System;
 using System.Collections.Generic;
@@ -33,35 +38,90 @@ namespace Hunted_Mobile.ViewModel {
             _view = view;
             _model = new Model.Map();
 
-            // Temporary code. Phone location can be obtained with: _view.MyLocationLayer.MyLocation
+            #region Temporary code (test data)
             _model.AddUser(new Thief(345) {
                 Name = "Henk",
                 Location = new Location() {
-                    Lattitude = 25.5,
-                    Longitude = 33.554325
+                    Lattitude = 51.7,
+                    Longitude = 5.55
                 }
             });
             _model.AddUser(new Police(346) {
                 Name = "Piet",
                 Location = new Location() {
-                    Lattitude = 24.223,
-                    Longitude = 34.0078
+                    Lattitude = 51.8,
+                    Longitude = 5.5
                 }
             });
             _model.PlayingUser = new Police(123) {
                 Name = "Hans",
                 Location = new Location() {
-                    Lattitude = 26.789,
-                    Longitude = 33.99912
+                    Lattitude = 51.7612,
+                    Longitude = 5.5140
                 }
             };
+            #endregion
+
+            LimitMapViewport(_model.PlayingUser.Location, 20000);
+            CenterMapOnLocation(_model.PlayingUser.Location, 45);
+
+            DisplayPins();
+
+            if(!CrossGeolocator.Current.IsListening) {
+                StartGPS();
+            }
+        }
+
+        private void MyLocationUpdated(object sender, PositionEventArgs e) {
+            _model.PlayingUser.Location = new Location(e.Position);
+
+            Mapsui.UI.Forms.Position mapsuiPosition = new Mapsui.UI.Forms.Position(e.Position.Latitude, e.Position.Longitude);
+            _view.MyLocationLayer.UpdateMyLocation(mapsuiPosition, true);
+            _view.MyLocationLayer.UpdateMySpeed(e.Position.Speed);
 
             DisplayPins();
         }
 
-        /**
-         * Displays pins for all game objects with a location
-         * **/
+        private async void StartGPS() {
+            //TODO settings may need to change
+            await CrossGeolocator.Current.StartListeningAsync(
+                TimeSpan.FromSeconds(1),
+                1,
+                false,
+                new ListenerSettings {
+                    ActivityType = ActivityType.Fitness,
+                    AllowBackgroundUpdates = false,
+                    DeferLocationUpdates = true,
+                    DeferralDistanceMeters = 1,
+                    DeferralTime = TimeSpan.FromSeconds(5),
+                    ListenForSignificantChanges = false,
+                    PauseLocationUpdatesAutomatically = true
+                }
+            );
+
+            CrossGeolocator.Current.PositionChanged += MyLocationUpdated;
+        }
+
+        private void CenterMapOnLocation(Location center, double zoomResolution) {
+            Point centerPoint = new Mapsui.UI.Forms.Position(center.Lattitude, center.Longitude).ToMapsui();
+            _view.Navigator.CenterOn(centerPoint);
+
+            _view.Navigator.NavigateTo(centerPoint, zoomResolution);
+        }
+
+        private void LimitMapViewport(Location center, int limit = 100000) {
+            _view.Map.Limiter = new ViewportLimiterKeepWithin();
+            Point centerPoint = new Mapsui.UI.Forms.Position(center.Lattitude, center.Longitude).ToMapsui();
+            Point min = new Point(centerPoint.X - limit, centerPoint.Y - limit);
+            Point max = new Point(centerPoint.X + limit, centerPoint.Y + limit);
+            _view.Map.Limiter.PanLimits = new BoundingBox(min, max);
+        }
+
+        private void ZoomMap(double resolution) {
+            _view.Navigator.ZoomTo(resolution);
+        }
+
+        /// Displays pins for all game objects with a location
         private void DisplayPins() {
             _view.Pins.Clear();
 
@@ -70,7 +130,7 @@ namespace Hunted_Mobile.ViewModel {
                 _view.Pins.Add(new Pin(_view) {
                     Label = user.Name,
                     Color = Xamarin.Forms.Color.Black,
-                    Position = new Position(user.Location.Lattitude, user.Location.Longitude),
+                    Position = new Mapsui.UI.Forms.Position(user.Location.Lattitude, user.Location.Longitude),
                     Scale = 0.666f,
                 });
             }
@@ -79,7 +139,7 @@ namespace Hunted_Mobile.ViewModel {
             _view.Pins.Add(new Pin(_view) {
                 Label = _model.PlayingUser.Name,
                 Color = Xamarin.Forms.Color.FromRgb(39, 96, 203),
-                Position = new Position(_model.PlayingUser.Location.Lattitude, _model.PlayingUser.Location.Longitude),
+                Position = new Mapsui.UI.Forms.Position(_model.PlayingUser.Location.Lattitude, _model.PlayingUser.Location.Longitude),
             });
         }
     }
