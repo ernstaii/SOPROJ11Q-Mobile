659dec2e91f4877468c4e3e2e0c7439ffecf4c52
Luke Derkzen <lderkzen@gmail.com>
2021-04-05 14:26:55 +0200

    Revert "Revert "Added branch #21""
    This reverts commit 15ee1c5c90f6d93faac9297101d7ed8afcdeb78e.

--- Hunted Mobile/Hunted Mobile/Hunted Mobile/Model/GameModels/Thief.cs
+++ Hunted Mobile/Hunted Mobile/Hunted Mobile/Model/GameModels/Thief.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Hunted_Mobile.Model.Game {
+namespace Hunted_Mobile.Model.GameModels {
     public class Thief : Player {
         public Thief(int id) : base(id) {
         }
