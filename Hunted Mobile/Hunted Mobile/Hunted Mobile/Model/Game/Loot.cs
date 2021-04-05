2ac6cedcdb64e204e745b5ca4a779b492b1b6ca9
Luke Derkzen <lderkzen@gmail.com>
2021-04-05 14:25:19 +0200

    Revert "Added branch #21"
    This reverts commit 31c8b360f3938cf0d69ec571a6a2415b439e1508.

---
+++ Hunted Mobile/Hunted Mobile/Hunted Mobile/Model/Game/Loot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hunted_Mobile.Model.Game {
+    public class Loot {
+        private readonly int _id;
+        public Location Location { get; set; }
+        public string Name { get; set; }
+
+        public Loot(int id) {
+            _id = id;
+        }
+    }
+}
