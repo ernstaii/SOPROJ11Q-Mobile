575199d345710869da114f3047e2fa935e85e190
imfi-jz <qasloot@avans.nl>
2021-04-09 11:08:06 +0200

    GpsService documentation

--- Hunted Mobile/Hunted Mobile/Hunted Mobile/Service/Gps/GpsService.cs
+++ Hunted Mobile/Hunted Mobile/Hunted Mobile/Service/Gps/GpsService.cs
@@ -10,12 +10,18 @@ using System.Text;
 namespace Hunted_Mobile.Service.Gps {
     public class GpsService {
         public delegate void LocationChangedEvent(Location location);
+        /// <summary>
+        /// Event called whenever the GPS has started and the current device's location changes
+        /// </summary>
         public event LocationChangedEvent LocationChanged;
 
         public GpsService() {
             CrossGeolocator.Current.PositionChanged += HandlerLocationChangedEvent;
         }
 
+        /// <summary>
+        ///  Handles PositionChanged events from CrossGeolocator.Current by calling the local LocationChanged event property
+        /// </summary>
         private void HandlerLocationChangedEvent(object sender, PositionEventArgs args) {
             LocationChanged(new Location(args.Position));
         }
