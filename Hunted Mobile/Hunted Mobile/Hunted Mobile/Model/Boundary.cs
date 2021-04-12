53424f1a010750b7151e1580c1f4ced9ea93d15a
imfi-jz <qasloot@avans.nl>
2021-04-12 16:14:22 +0200

    Added some specific point getters to Boundary

--- Hunted Mobile/Hunted Mobile/Hunted Mobile/Model/Boundary.cs
+++ Hunted Mobile/Hunted Mobile/Hunted Mobile/Model/Boundary.cs
@@ -17,5 +17,70 @@ namespace Hunted_Mobile.Model {
             }
             return polygon;
         }
+
+        public Location GetCenter() {
+            if(Points.Count > 0) {
+                Location center = new Location(0, 0);
+
+                foreach(Location location in Points) {
+                    center.Latitude += location.Latitude;
+                    center.Longitude += location.Longitude;
+                }
+
+                center.Latitude /= Points.Count;
+                center.Longitude /= Points.Count;
+
+                return center;
+            }
+            else return null;
+        }
+
+        public Location GetPointFurthestNorth() {
+            Location furthestNorth = null;
+
+            foreach(Location location in Points) {
+                if(furthestNorth == null || location.Longitude > furthestNorth.Longitude) {
+                    furthestNorth = location;
+                }
+            }
+
+            return furthestNorth;
+        }
+
+        public Location GetPointFurthestSouth() {
+            Location furthestSouth = null;
+
+            foreach(Location location in Points) {
+                if(furthestSouth == null || location.Longitude < furthestSouth.Longitude) {
+                    furthestSouth = location;
+                }
+            }
+
+            return furthestSouth;
+        }
+
+        public Location GetPointFurthestEast() {
+            Location furthestEast = null;
+
+            foreach(Location location in Points) {
+                if(furthestEast == null || location.Latitude > furthestEast.Latitude) {
+                    furthestEast = location;
+                }
+            }
+
+            return furthestEast;
+        }
+
+        public Location GetPointFurthestWest() {
+            Location furthestWest = null;
+
+            foreach(Location location in Points) {
+                if(furthestWest == null || location.Latitude < furthestWest.Latitude) {
+                    furthestWest = location;
+                }
+            }
+
+            return furthestWest;
+        }
     }
 }
