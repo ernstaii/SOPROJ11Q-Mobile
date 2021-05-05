3505753c3fec5eda18c5d27617a22d894985c471
imfi-jz <qasloot@avans.nl>
2021-05-05 15:49:44 +0200

    Resource models

---
+++ Hunted Mobile/Hunted Mobile/Hunted Mobile/Model/Resource/LazyLoadedResource.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Threading.Tasks;
+
+namespace Hunted_Mobile.Model.Resource {
+    class LazyLoadedResource : Resource {
+        private readonly Uri uri;
+        private byte[] data;
+        private readonly Task<byte[]> dataGetter;
+
+        public string AbsolutePath => uri.AbsoluteUri;
+
+        public byte[] Data {
+            get {
+                if(dataGetter.Status == TaskStatus.Created) {
+                    dataGetter.Start();
+                    data = dataGetter.Result;
+                    return data;
+                }
+                else return data;
+            }
+        }
+
+        public LazyLoadedResource(Uri uriToResource, Task<byte[]> dataGetter) {
+            uri = uriToResource;
+            this.dataGetter = dataGetter;
+        }
+    }
+}
---
+++ Hunted Mobile/Hunted Mobile/Hunted Mobile/Model/Resource/Resource.cs
@@ -0,0 +1,6 @@
+﻿namespace Hunted_Mobile.Model.Resource {
+    public interface Resource {
+        string AbsolutePath { get; }
+        byte[] Data { get; }
+    }
+}
