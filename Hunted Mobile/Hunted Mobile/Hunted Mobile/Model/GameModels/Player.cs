d86b985ecf2f6a3b00560495c9081b987404aa51
Ghoster524 <60692171+Ghoster524@users.noreply.github.com>
2021-05-05 23:21:45 +0200

    fix: Updated IsCaught boolean with different options

--- Hunted Mobile/Hunted Mobile/Hunted Mobile/Model/GameModels/Player.cs
+++ Hunted Mobile/Hunted Mobile/Hunted Mobile/Model/GameModels/Player.cs
@@ -13,7 +13,7 @@ namespace Hunted_Mobile.Model.GameModels {
         [MinLength(3, ErrorMessage = "De gebruikersnaam heeft een minimale lengte van 3 karaktes")]
         [MaxLength(50, ErrorMessage = "De gebruikersnam geeft een maximale lengte van 50 karaktes")]
         public string UserName { get; set; }
-        public bool IsCaught => CaughtAt != null && !CaughtAt.Equals("");
+        public bool IsCaught => CaughtAt != null && !CaughtAt.Equals("") || Status == "caught";
         public bool IsFree => !IsCaught;
         public string CaughtAt { get; set; }
         public string Status { get; set; }
