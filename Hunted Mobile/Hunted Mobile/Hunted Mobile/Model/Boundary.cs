cbc533d21ce167f9e3c8430fe21604af94931db6
imfi-jz <qasloot@avans.nl>
2021-05-12 15:16:53 +0200

    Within boundary method

--- Hunted Mobile/Hunted Mobile/Hunted Mobile/Model/Boundary.cs
+++ Hunted Mobile/Hunted Mobile/Hunted Mobile/Model/Boundary.cs
@@ -47,5 +47,41 @@ namespace Hunted_Mobile.Model {
             }
             return diameter;
         }
+
+        // http://www.alienryderflex.com/polygon/
+        // 
+        //  The function will return true if the point x,y (location) is inside the
+        //  polygon (boundary), or false if it is not.
+        //  If the point is exactly on the edge of the polygon, then the function
+        //  may return true or false.
+        //
+        //  Note that division by zero is avoided because the division is protected
+        //  by the "if" clause which surrounds it.
+        public bool Contains(Location location) {
+            int j = Points.Count - 1;
+            bool oddNodes = false;
+
+            var testingX = location.Latitude;
+            var testingY = location.Longitude;
+
+            for(int i = 0; i < Points.Count; i++) {
+                var currentBoundaryPointY = Points[i].Longitude;
+                var currentBoundaryPointX = Points[i].Latitude;
+                var previousBoundaryPointY = Points[j].Longitude;
+                var previousBoundaryPointX = Points[j].Latitude;
+                if((currentBoundaryPointY < testingY && previousBoundaryPointY >= testingY
+                    || previousBoundaryPointY < testingY && currentBoundaryPointY >= testingY)
+                    && (currentBoundaryPointX <= testingX || previousBoundaryPointX <= testingX)) {
+                    if(currentBoundaryPointX + 
+                        (testingY - currentBoundaryPointY) / (previousBoundaryPointY - currentBoundaryPointY) *
+                        (previousBoundaryPointX - currentBoundaryPointX) < testingX) {
+                        oddNodes = !oddNodes;
+                    }
+                }
+                j = i;
+            }
+
+            return oddNodes;
+        }
     }
 }
