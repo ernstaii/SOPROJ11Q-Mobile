38c8747a605ced45a91142ea50e064cf8c47ae8b
imfi-jz <qasloot@avans.nl>
2021-05-18 11:11:17 +0200

    Refactor event data for expandability

---
+++ Hunted Mobile/Hunted Mobile/Hunted Mobile/Model/Response/EventData.cs
@@ -0,0 +1,17 @@
+﻿using Hunted_Mobile.Model.GameModels;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hunted_Mobile.Model.Response {
+    public class EventData : ResponseData {
+        public string Message { get; set; }
+        public int TimeLeft { get; set; }
+    }
+
+    public class IntervalEventData : EventData {
+        public Player[] Players { get; set; }
+        public Loot[] Loot { get; set; }
+    }
+}
--- Hunted Mobile/Hunted Mobile/Hunted Mobile/Model/Response/Json/EventDataModels.cs
+++ Hunted Mobile/Hunted Mobile/Hunted Mobile/Model/Response/Json/EventDataModels.cs
@@ -4,10 +4,13 @@ using System.Text;
 
 namespace Hunted_Mobile.Model.Response.Json {
 #pragma warning disable IDE1006 // Naming Styles
-    public struct IntervalEventData : JsonResponseData {
+    public class EventData : JsonResponseData {
+        public string message;
+        public int timeLeft;
+    }
+
+    public class IntervalEventData : EventData {
         public UserData[] users;
         public LootData[] loot;
-        public string message;
-        public int? timeLeft;
     }
 }
