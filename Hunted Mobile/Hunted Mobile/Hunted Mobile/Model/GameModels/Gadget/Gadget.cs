a68c5e9a65bfdebdb31ce44549870a3e9ba7c98c
imfi-jz <qasloot@avans.nl>
2021-05-26 13:11:19 +0200

    Gadget repository

--- Hunted Mobile/Hunted Mobile/Hunted Mobile/Model/GameModels/Gadget/Gadget.cs
+++ Hunted Mobile/Hunted Mobile/Hunted Mobile/Model/GameModels/Gadget/Gadget.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Hunted_Mobile.Repository;
+
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Hunted_Mobile.Model.GameModels.Gadget {
     public abstract class Gadget {
-        public void Use() {
-            // TODO: remove gadget from database and from player's collection
+        public int Id { get; set; }
+
+        public async Task Use(Player user) {
+            await UnitOfWork.Instance.GadgetRepository.DecreaseGadgetAmount(user.Id, Id);
+            user.Gadgets.Remove(this);
         }
 
         public abstract bool CanBeUsed();
---
+++ Hunted Mobile/Hunted Mobile/Hunted Mobile/Repository/GadgetRepository.cs
@@ -0,0 +1,23 @@
+﻿using Hunted_Mobile.Model.GameModels;
+using Hunted_Mobile.Model.GameModels.Gadget;
+using Hunted_Mobile.Service;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hunted_Mobile.Repository {
+    public class GadgetRepository {
+        public GadgetRepository() {
+
+        }
+
+        public async Task<bool> DecreaseGadgetAmount(int playerId, int gadgetId) {
+            var response = new HttpClientResponse();
+            await response.Convert(HttpClientRequestService.Patch(HttpClientRequestService.GetUrl($"users/{playerId}/gadgets/{gadgetId}")));
+
+            return response.IsSuccessful;
+        }
+    }
+}
