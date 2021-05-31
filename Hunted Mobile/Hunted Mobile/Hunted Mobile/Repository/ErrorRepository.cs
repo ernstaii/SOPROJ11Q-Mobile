bdc9438daba68ba7bdca3574e08cc88ba6fb84be
Jesse Stupers <49638126+Stupers@users.noreply.github.com>
2021-05-31 12:18:07 +0200

    Added default values

--- Hunted Mobile/Hunted Mobile/Hunted Mobile/Repository/ErrorRepository.cs
+++ Hunted Mobile/Hunted Mobile/Hunted Mobile/Repository/ErrorRepository.cs
@@ -11,22 +11,29 @@ namespace Hunted_Mobile.Repository {
     public class ErrorRepository {
         public async void Create(Exception e) {
             var w32ex = e as Win32Exception;
+            int errorCode = 137;
+
+            if(w32ex == null) {
+                if(e.InnerException != null) {
+                    w32ex = e.InnerException as Win32Exception;
+                    errorCode = w32ex.ErrorCode;
+                }
+            }
 
             var response = new HttpClientResponse();
             await response.Convert(HttpClientRequestService.Create("app-errors", new {
-                error_id = w32ex.ErrorCode,
-                message = e.Message,
-                stacktrace = e.StackTrace
+                error_id = errorCode,
+                message = e.Message ?? "Er was geen message",
+                stacktrace = e.StackTrace ?? "Er was geen stacktrace"
             }));
         }
 
         public async void Create(PusherException e) {
-
             var response = new HttpClientResponse();
             await response.Convert(HttpClientRequestService.Create("app-errors", new {
-                error_id = e.PusherCode,
-                message = e.Message,
-                stacktrace = e.StackTrace
+                error_id = int.Parse(e.PusherCode.ToString()),
+                message = e.Message ?? "Er was geen message [PUSHER ERROR]",
+                stacktrace = e.StackTrace ?? "Er was geen stacktrace [PUSHER ERROR]"
             }));
         }
     }
